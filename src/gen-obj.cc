dbc288f341130ee38b858e07aef89067b6587ef6
xnscdev <xnscdev@gmail.com>
2021-03-20 10:32:27 -0700

    Section switching/creation with flags

--- src/gen-obj.cc
+++ src/gen-obj.cc
@@ -19,6 +19,70 @@
 #include "assemble.hh"
 #include "gen-obj.hh"
 
+static Elf32_Word
+elf_section_type (std::string section)
+{
+  if (section == ".bss")
+    return SHT_NOBITS;
+  if (section == ".note")
+    return SHT_NOTE;
+  if (section == ".hash")
+    return SHT_HASH;
+  if (section == ".dynamic")
+    return SHT_DYNAMIC;
+  return SHT_PROGBITS;
+}
+
+static Elf32_Word
+elf_section_flags (std::string section)
+{
+  if (section.rfind (".data", 0) == 0 || section == ".bss")
+    return SHF_ALLOC | SHF_WRITE;
+  if (section.rfind (".rodata", 0) == 0 || section == ".hash")
+    return SHF_ALLOC;
+  if (section == ".text")
+    return SHF_ALLOC | SHF_EXECINSTR;
+  return 0;
+}
+
+static Elf32_Word
+elf_section_align (std::string section)
+{
+  /* Not required, but for best performance */
+  if (section == ".text")
+    return 16;
+  if (section.rfind (".data", 0) == 0 || section.rfind (".rodata", 0) == 0
+      || section == ".bss")
+    return 4;
+  return 1;
+}
+
+static uint32_t
+coff_section_flags (std::string section)
+{
+  uint32_t align = IMAGE_SCN_ALIGN_1BYTES;
+  if (section == ".text")
+    align = IMAGE_SCN_ALIGN_16BYTES;
+  if (section.rfind (".data", 0) == 0 || section.rfind (".rodata", 0) == 0
+      || section == ".bss")
+    align = IMAGE_SCN_ALIGN_4BYTES;
+
+  uint32_t perms = IMAGE_SCN_MEM_READ;
+  if (section.rfind (".rodata", 0) != 0 && section != ".hash"
+      && section != ".text")
+    perms |= IMAGE_SCN_MEM_WRITE;
+  if (section == ".text")
+    perms |= IMAGE_SCN_MEM_EXECUTE;
+
+  uint32_t type = IMAGE_SCN_CNT_INITIALIZED_DATA;
+  if (perms & IMAGE_SCN_MEM_EXECUTE)
+    type = IMAGE_SCN_CNT_CODE;
+  if (section == ".bss")
+    type = IMAGE_SCN_CNT_UNINITIALIZED_DATA;
+
+  return align | perms | type;
+}
+
 bool
 Object::elf_assemble (AsmInst *inst)
 {
@@ -55,6 +119,34 @@ void
 Object::switch_section (std::string new_sect)
 {
   section = new_sect;
+  uint32_t id;
+  switch (type)
+    {
+    case ObjectFileFormat::ELF32:
+      id = elf.search_section (section);
+      break;
+    case ObjectFileFormat::COFF32:
+      id = coff.search_section (section);
+      break;
+    default:
+      return; /* Sections not supported in raw binary, or invalid type */
+    }
+  if (id == 0)
+    {
+      switch (type)
+	{
+	case ObjectFileFormat::ELF32:
+	  elf.add_section (section, elf_section_type (section),
+			   elf_section_flags (section),
+			   elf_section_align (section));
+	  break;
+	case ObjectFileFormat::COFF32:
+	  coff.add_section (section, coff_section_flags (section));
+	  break;
+	default:
+	  break; /* -Wswitch */
+	}
+    }
 }
 
 bool
